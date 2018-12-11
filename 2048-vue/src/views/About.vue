32982a152630481b1caf44bfa4744639227d52f7
郝洋洋 <1622072353@qq.com>
2018-12-11 16:26:50 +0800

    window.onload事件

--- 2048-vue/src/views/About.vue
+++ 2048-vue/src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p @click="pClick(item , index)"
+       v-for="(item,index) in pList"
+       :key="index">{{item.text}}</p>
+       <p style="background-color: lightgrey;">
+         <ol style="color:white">
+           <li>lalalal</li>
+         </ol>
+       </p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      pList: [
+        {
+          id: '1',
+          text: '第一段'
+        },
+        {
+          id: '2',
+          text: '第二段'
+        },
+        {
+          id: '3',
+          text: '第三段'
+        }
+      ]
+    }
+  },
+  methods: {
+    pClick(item, index) {
+      console.log(item, index)
+    }
+  }
+}
+</script>
